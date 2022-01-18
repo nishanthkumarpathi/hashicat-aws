95fa2129d641a02f4ca33a569433acf670d6fb3d
Nishanth <pathinishanth@gmail.com>
2022-01-18 21:18:51 +0000

    added s3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,12 @@
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket_prefix = "nishanth"
+  bucket = bucket_preofix"-my-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+}
