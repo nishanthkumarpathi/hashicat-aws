a215399d352723a48d3ca346ee67077aec3ba0b5
Nishanth <pathinishanth@gmail.com>
2022-01-18 21:45:26 +0000

    added s3 version and upgrade

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
-  version = 2.2.0
+  version = "=2.2.0"
 
   #bucket_prefix = "nishanth"
   #bucket = "${bucket_prefix}-my-s3-bucket"
